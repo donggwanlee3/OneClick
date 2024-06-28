7c0665b6776123ca8f329be2115279ae0c258bc4
David Lee <donggwanlee2@berley.edu>
2024-06-27 23:16:55 -0700

    Initial Commit

---
+++ OneClick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OneClick
+//
+//  Created by donggwan lee on 6/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OneClick/OneClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneClickApp.swift
+//  OneClick
+//
+//  Created by donggwan lee on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct OneClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
